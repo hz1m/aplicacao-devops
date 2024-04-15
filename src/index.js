70d7109c42687b865aff14032243a7badc39af84
Huldreich <hudimelo1806@gmail.com>
2024-04-14 22:44:16 -0300

    adiciona endpoint /integrantes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname });
 });
 
+app.get('/integrantes', (req, res) => {
+  const data = {
+    'integrantes': [
+      {'nome': 'Nome Sobrenome1'},
+      {'nome': 'Nome Sobrenome2'},
+      // Adicione os outros integrantes aqui
+    ]
+  };
+  res.json(data);
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
